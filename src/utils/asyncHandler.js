3372aa396c2e0c5128f15a136d686f4d66d84064
ribhugautam <gautamribhu@gmail.com>
2024-06-16 08:36:57 +0530

    Set-up first controller and route for user registration

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,16 @@
 const asyncHandler = (fn) => async (req, res, next) => {
   try {
+    // Execute the asynchronous function 'fn' with the provided parameters
     await fn(req, res, next);
   } catch (error) {
-    res
-      .status(err.code || 500)
-      .json({ success: false, message: error.message });
+    // If an error occurs during the execution of 'fn', handle it here
+    const statusCode = error.code || 500; // Determine the HTTP status code
+    const errorMessage = error.message; // Extract the error message
+
+    // Send a JSON response indicating failure with the error details
+    res.status(statusCode).json({ success: false, message: errorMessage });
   }
 };
 
+// Export the asyncHandler function to be used elsewhere in the application
 export { asyncHandler };
