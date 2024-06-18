0da02c20cdf433d6e346cbb1cf2fc0f9b5a914cf
ribhugautam <gautamribhu@gmail.com>
2024-06-18 20:14:12 +0530

    added subscription schema

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscription: [
+      {
+        type: mongoose.Schema.Types.ObjectId, //one who is subscribing
+        ref: "User",
+      },
+    ],
+    channel: {
+      type: mongoose.Schema.Types.ObjectId, //one whom is being subscribed
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
